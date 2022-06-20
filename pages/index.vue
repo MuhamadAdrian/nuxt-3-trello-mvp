1e825653f42eab006d4708b23774a40bdfc8225a
MuhamadAdrian <madrian.septiana@gmail.com>
2022-06-20 14:11:58 +0700

    refactor, feat: using typescript, design workspace item

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,8 @@
     <h3>Workspace</h3>
     <input type="text" v-model="newWorkspaceName">
     <button @click="createWorkspace">create a Workspace</button>
-    <ul>
-        <li v-for="workspace in workspaces" :key="workspace.id">
+    <ul class="workspace-list">
+        <li class="workspace-card" v-for="workspace in workspaces" :key="workspace.id">
             {{ workspace.name }}
         </li>
     </ul>
@@ -24,7 +24,10 @@
 
     const newWorkspaceName = ref<string>('');
 
-    const workspaces = ref<workSpace[]>([])
+    const workspaces = ref<workSpace[]>([
+        { id: 123, name: 'Test' },
+        { id: 234, name: 'Expremient' },
+    ])
 
     const createWorkspace = () => {
         const randomId = Math.floor(Math.random() * 100);
@@ -37,5 +40,18 @@
 </script>
 
 <style scoped>
-
+.workspace-card {
+  display: block;
+  border: 2px solid #222;
+  border-radius: 4px;
+  padding: 2rem;
+  margin-bottom: 1rem;
+}
+.workspace-list {
+  margin-left: 0;
+  padding-left: 0;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-column-gap: 10px;
+}
 </style>
